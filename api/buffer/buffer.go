21327438eac393f73750620bb5eb1f4f28f7f660
Joseph Cocozza <jcocozza47@gmail.com>
2025-01-25 09:28:28 -0500

    docs: adding some comments

--- api/buffer/buffer.go
+++ api/buffer/buffer.go
@@ -10,23 +10,24 @@ type Buffer interface {
 	DeleteChar()
 	// insert a new line by creating a new line and moving the cursor down
 	InsertNewLine()
-	// move cursor up
-	Up()
-	// move cursor down
-	Down()
-	// move cursor left
-	Left()
-	// move cursor right
-	Right()
-	// X position of the cursor in the buffer
-	X() int
-	// Y position of the cursor in the buffer
-	Y() int
+
+	/* cursor stuff */
+
+	Up() // move cursor up
+	Down() // move cursor down
+	Left() // move cursor left
+	Right() // move cursor right
+	StartLine() // move to start of line cursor is currently on
+	EndLine() // move to end of line cursor is currently on
+	X() int // x position of cursor
+	Y() int // y position of cursor
+
 	// number of rows read into buffer
 	NumRows() int
 	// total number of rows in the file
 	TotalRows() int
 	Row(num int) []byte
+	// if the buffer has been edited or not
 	Dirty() bool
 	// return the name of the buffer
 	Name() string
