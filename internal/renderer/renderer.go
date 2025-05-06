b7706eb1c6e8796d3cdb5011624e6a7197d68cdf
Joseph Cocozza <jcocozza47@gmail.com>
2025-05-06 09:33:15 -0400

    feat: basic renderer

--- internal/renderer/renderer.go
+++ internal/renderer/renderer.go
@@ -1,8 +1,75 @@
 package renderer
 
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/jcocozza/jte/internal/term"
+)
+
 type Renderer interface {
 	Setup() error
 	Exit(msg string)
 	ExitErr(err error)
 	Render()
+}
+
+type TextRenderer struct {
+	rw *term.RawMode
+
+	screenrows int
+	//initscreenrows int
+	screencols int
+
+	rowoffset int
+	coloffset int
+
+	// the content that is actually rendered to the screen
+	abuf abuf
+
+	logger *slog.Logger
+}
+
+func NewTextRenderer(l *slog.Logger) *TextRenderer {
+	return &TextRenderer{
+		abuf:   abuf{},
+		logger: l.WithGroup("renderer"),
+	}
+}
+
+func (r *TextRenderer) Setup() error {
+	rw, err := term.EnableRawMode()
+	if err != nil {
+		return err
+	}
+	r.rw = rw
+	return nil
+}
+
+func (r *TextRenderer) cleanup() {
+	r.abuf.Append([]byte("\x1b[2J")) // clear entire screen
+	r.abuf.Flush()
+	if r.rw == nil {
+		return
+	}
+	err := r.rw.Restore()
+	if err != nil {
+		r.logger.Error("failed to restore terminal", "error", err)
+	}
+}
+
+func (r *TextRenderer) ExitErr(err error) {
+	r.cleanup()
+	fmt.Fprintln(os.Stderr, err.Error())
+	os.Exit(1)
+}
+
+func (r *TextRenderer) Exit(msg string) {
+	r.cleanup()
+	if msg == "" {
+		os.Exit(0)
+	}
+	fmt.Fprintln(os.Stdout, msg)
+	os.Exit(0)
 }
