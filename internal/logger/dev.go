0cf2e3c899cbb3cb591fb438047efd64e72b7c35
Joseph Cocozza <jcocozza47@gmail.com>
2025-07-13 12:55:24 -0400

    feat: remove time for now

--- internal/logger/dev.go
+++ internal/logger/dev.go
@@ -8,12 +8,33 @@ import (
 	"os"
 )
 
+//func newLogger() *slog.Logger {
+//	f, err := os.OpenFile("jte.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+//	if err != nil {
+//		panic(err)
+//	}
+//	handler := slog.NewTextHandler(f, &slog.HandlerOptions{Level: slog.LevelDebug})
+//	l := slog.New(handler)
+//	return l
+//}
+
 func newLogger() *slog.Logger {
 	f, err := os.OpenFile("jte.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
-	handler := slog.NewTextHandler(f, &slog.HandlerOptions{Level: slog.LevelDebug})
+
+	handler := slog.NewTextHandler(f, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				// Remove the time attribute
+				return slog.Attr{}
+			}
+			return a
+		},
+	})
+
 	l := slog.New(handler)
 	return l
 }
